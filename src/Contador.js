0ad8f81447c6ba91bb2dda7f13be4d39cb1dbd58
busterlnk <73650574+busterlnk@users.noreply.github.com>
2022-09-26 21:33:49 +0200

    Arreglo conexion con bdd
    Ahora se logra conectar la bdd completa, pero solo se muestra la primera hasta que se haga una modificacion en el codigo jsx

--- src/Contador.js
+++ src/Contador.js
@@ -19,9 +19,9 @@ const Contador = () => {
     if (!firebase.apps.length) {
         firebase.initializeApp(Config)
     }
-    const list = [];
     useEffect(() => {
         firebase.database().ref().on('value', (puntos) => {
+            const list = [];
             puntos.forEach((punto) => {
                 list.push(punto.val())
                 cambiarPuntoFinal(list)
@@ -30,94 +30,93 @@ const Contador = () => {
     }, []);
     return (
         <div>
-            {
-                puntoFinal.map((punto, i) => {
-                    console.log(punto.P11S);
-                    return (<table className="tabla-puntos-1" key={punto.PAREJA1+i}>
+            {Array.isArray(puntoFinal) ? puntoFinal.map((partido, index) => {
+                console.log(partido)
+                    return (<table className="tabla-puntos-1" key={partido+index}>
                         <tbody>
                             <tr>
-                                {punto.PAREJA1 !== undefined && punto.SAQUE1 !== undefined ?
-                                    <th className="parejas" colSpan="1">{punto.PAREJA1.replace(/['\\"]+/g, '').toUpperCase()}</th> :
+                                {partido.PAREJA1 !== undefined && partido.SAQUE1 !== undefined ?
+                                    <th className="parejas" colSpan="1">{partido.PAREJA1.replace(/['\\"]+/g, '').toUpperCase()}</th> :
                                     console.log()
                                 }
 
-                                {punto.SAQUE1 !== undefined ?
-                                    <th className="saque">{punto.SAQUE1.replace(/['"]+/g, '')}</th> :
+                                {partido.SAQUE1 !== undefined ?
+                                    <th className="saque">{partido.SAQUE1.replace(/['"]+/g, '')}</th> :
                                     <th className="saque"></th>
                                 }
-                                {(punto.P11S !== '""' || punto.P21S !== '""') && punto.P11S !== undefined ?
-                                    <th className="set">{punto.P11S.replace(/['"]+/g, '')}</th>
+                                {(partido.P11S !== '""' || partido.P21S !== '""') && partido.P11S !== undefined ?
+                                    <th className="set">{partido.P11S.replace(/['"]+/g, '')}</th>
                                     :
                                     console.log()
                                 }
 
-                                {(punto.P12S !== '""' || punto.P22S !== '""') && punto.P12S !== undefined ?
-                                    <th className="set">{punto.P12S.replace(/['"]+/g, '')}</th>
+                                {(partido.P12S !== '""' || partido.P22S !== '""') && partido.P12S !== undefined ?
+                                    <th className="set">{partido.P12S.replace(/['"]+/g, '')}</th>
                                     :
                                     console.log()
                                 }
 
-                                {(punto.P13S !== '""' || punto.P23S !== '""') && punto.P13S !== undefined ?
-                                    <th className="set">{punto.P13S.replace(/['"]+/g, '')}</th>
+                                {(partido.P13S !== '""' || partido.P23S !== '""') && partido.P13S !== undefined ?
+                                    <th className="set">{partido.P13S.replace(/['"]+/g, '')}</th>
                                     :
                                     console.log()
                                 }
 
-                                {(punto.P1PS !== '""' || punto.P2PS !== '""' || punto.PUNTUACION === '"TBR"')
-                                    && (punto.SAQUE1 !== '""' || punto.SAQUE2 !== '""' || punto.P1PS !== '"0"' || punto.P2PS !== '"0"')
-                                    && punto.P1PS !== undefined ?
-                                    punto.PUNTUACION === '"ORO"' ?
-                                        <th className="oro">{punto.P1PS.replace(/['"]+/g, '')}</th> : punto.PUNTUACION === '"TBR"' ?
-                                            <th className="tbr">{punto.P1PS.replace(/['"]+/g, '')}</th> :
-                                            <th className="puntos">{punto.P1PS.replace(/['"]+/g, '')}</th>
+                                {(partido.P1PS !== '""' || partido.P2PS !== '""' || partido.PUNTUACION === '"TBR"')
+                                    && (partido.SAQUE1 !== '""' || partido.SAQUE2 !== '""' || partido.P1PS !== '"0"' || partido.P2PS !== '"0"')
+                                    && partido.P1PS !== undefined ?
+                                    partido.PUNTUACION === '"ORO"' ?
+                                        <th className="oro">{partido.P1PS.replace(/['"]+/g, '')}</th> : partido.PUNTUACION === '"TBR"' ?
+                                            <th className="tbr">{partido.P1PS.replace(/['"]+/g, '')}</th> :
+                                            <th className="puntos">{partido.P1PS.replace(/['"]+/g, '')}</th>
 
                                     :
                                     console.log()
                                 }
                             </tr>
                             <tr>
-                                {punto.PAREJA2 !== undefined && punto.SAQUE2 !== undefined ?
-                                    <th className="parejas" colSpan="1">{punto.PAREJA2.replace(/['\\"]+/g, '').toUpperCase()}</th> :
+                                {partido.PAREJA2 !== undefined && partido.SAQUE2 !== undefined ?
+                                    <th className="parejas" colSpan="1">{partido.PAREJA2.replace(/['\\"]+/g, '').toUpperCase()}</th> :
                                     console.log()
                                 }
-                                {punto.SAQUE2 !== undefined ?
-                                    <th className="saque">{punto.SAQUE2.replace(/['"]+/g, '')}</th> :
+                                {partido.SAQUE2 !== undefined ?
+                                    <th className="saque">{partido.SAQUE2.replace(/['"]+/g, '')}</th> :
                                     <th className="saque"></th>
                                 }
 
-                                {(puntoFinal.P11S !== '""' || puntoFinal.P21S !== '""') && puntoFinal.P11S !== undefined ?
-                                    <th className="set">{puntoFinal.P21S.replace(/['"]+/g, '')}</th>
+                                {(partido.P11S !== '""' || partido.P21S !== '""') && partido.P11S !== undefined ?
+                                    <th className="set">{partido.P21S.replace(/['"]+/g, '')}</th>
                                     :
                                     console.log()
                                 }
 
-                                {(puntoFinal.P12S !== '""' || puntoFinal.P22S !== '""') && puntoFinal.P22S !== undefined ?
-                                    <th className="set">{puntoFinal.P22S.replace(/['"]+/g, '')}</th>
+                                {(partido.P12S !== '""' || partido.P22S !== '""') && partido.P22S !== undefined ?
+                                    <th className="set">{partido.P22S.replace(/['"]+/g, '')}</th>
                                     :
                                     console.log()
                                 }
 
-                                {(puntoFinal.P13S !== '""' || puntoFinal.P23S !== '""') && puntoFinal.P23S !== undefined ?
-                                    <th className="set">{puntoFinal.P23S.replace(/['"]+/g, '')}</th>
+                                {(partido.P13S !== '""' || partido.P23S !== '""') && partido.P23S !== undefined ?
+                                    <th className="set">{partido.P23S.replace(/['"]+/g, '')}</th>
                                     :
                                     console.log()
                                 }
 
-                                {(puntoFinal.P1PS !== '""' || puntoFinal.P2PS !== '""' || puntoFinal.PUNTUACION === '"TBR"')
-                                    && (puntoFinal.SAQUE1 !== '""' || puntoFinal.SAQUE2 !== '""' || puntoFinal.P1PS !== '"0"' || puntoFinal.P2PS !== '"0"')
-                                    && puntoFinal.P2PS !== undefined ?
-                                    puntoFinal.PUNTUACION === '"ORO"' ?
-                                        <th className="oro">{puntoFinal.P2PS.replace(/['"]+/g, '')}</th> :
-                                        puntoFinal.PUNTUACION === '"TBR"' ?
-                                            <th className="tbr">{puntoFinal.P2PS.replace(/['"]+/g, '')}</th> :
-                                            <th className="puntos">{puntoFinal.P2PS.replace(/['"]+/g, '')}</th>
+                                {(partido.P1PS !== '""' || partido.P2PS !== '""' || partido.PUNTUACION === '"TBR"')
+                                    && (partido.SAQUE1 !== '""' || partido.SAQUE2 !== '""' || partido.P1PS !== '"0"' || partido.P2PS !== '"0"')
+                                    && partido.P2PS !== undefined ?
+                                    partido.PUNTUACION === '"ORO"' ?
+                                        <th className="oro">{partido.P2PS.replace(/['"]+/g, '')}</th> :
+                                        partido.PUNTUACION === '"TBR"' ?
+                                            <th className="tbr">{partido.P2PS.replace(/['"]+/g, '')}</th> :
+                                            <th className="puntos">{partido.P2PS.replace(/['"]+/g, '')}</th>
                                     :
                                     console.log()
                                 }
                             </tr>
                         </tbody>
                     </table>)
-                })}
+                }) :null}
             {/* <table className="tabla-puntos-2">
             <tbody>
                     <tr>
